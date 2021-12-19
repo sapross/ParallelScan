99b1d3ba56fa7eee3ed0ea29a80612bf401a52f4
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2021-12-19 13:38:30 +0100

    Basic test template for the scan pattern.

--- include/scan.hpp
+++ include/scan.hpp
@@ -3,4 +3,11 @@
 #include <cstddef>
 #include <stdint.h>
 
-
+namespace naive
+{
+namespace sequential
+{
+template<class ForwardIt1, class ForwardIt2>
+ForwardIt2 inclusive_scan(ForwardIt1 first, ForwardIt1 last, ForwardIt2 d_first);
+}
+}; // namespace naive
--- test/test.cpp
+++ test/test.cpp
@@ -1,21 +1,38 @@
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_FAST_COMPILE
+#include "logrange_generator"
 #include <catch2/catch.hpp>
+
+#include <algortihm>
+#include <numeric>
+#include <random>
 
 #include "scan.hpp"
 
-TEST_CASE("Test")
+std::default_random_engine         generator;
+std::uniform_int_distribution<int> distribution(1, 10);
+auto                               rand = std::bind(distribution, generator);
+
+TEST_CASE("Inclusive Scan Test")
 {
     // Test parameters
-    size_t N = GENERATE(1, 2, 3, 8, 13, 16, 23, 32);
-    using namespace dissemination_Barrier;
-    
+    size_t N = LogRange(1, 100, 10);
     // Logging of parameters
     CAPTURE(N);
 
-    
-    //Tests
-    REQUIRE(1==1);
-    CHECK(true==true);
-}
+    std::vector<int> data(N, 0);
+    std::for_each(data.begin(), data.end(), [=](auto& element) { element = rand(); });
 
+    std::vector<int> reference(N, 0);
+    std::inclusive_scan(data.begin(), data.end(), reference.begin());
+
+    // Tests
+    SECTION("Naive Sequential")
+    {
+        std::vector<int> result(N, 0);
+        naive::sequential::inclusive_scan(data.begin(), data.end(), result.begin());
+        REQUIRE_THAT(result, Catch::Matchers::Equals(reference));
+    }
+    // SECTION("Naive Up-Down-Sweep") { ; }
+    // SECTION("Naive Tiled") { ; }
+}
