ed94f731a1b42e8e955971217ee8f4cdd1a5063d
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2021-12-19 14:51:41 +0100

    Added naive sequential inclusive scan.

--- include/scan.hpp
+++ include/scan.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <algorithm>
+#include <numeric>
+
 #include <cstddef>
 #include <stdint.h>
 
@@ -8,6 +11,16 @@ namespace naive
 namespace sequential
 {
 template<class ForwardIt1, class ForwardIt2>
-ForwardIt2 inclusive_scan(ForwardIt1 first, ForwardIt1 last, ForwardIt2 d_first);
+ForwardIt2 inclusive_scan(ForwardIt1 first, ForwardIt1 last, ForwardIt2 d_first)
+{
+    using InputType = typename std::iterator_traits<ForwardIt1>::value_type;
+    InputType sum   = InputType(0);
+    for (auto i = first; i != last; i++)
+    {
+        sum += *i;
+        *(d_first++) = sum;
+    }
+    return d_first;
 }
+} // namespace sequential
 }; // namespace naive
