88c265fb3dc02a5191264293ad41c77e2bfa9fa9
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2021-12-20 10:54:08 +0100

    Added tests to segmented scan variants. Tests are negative.

--- include/scan.hpp
+++ include/scan.hpp
@@ -12,14 +12,12 @@ namespace sequential
 // ----------------------------------------------------------------------------------
 //  Inclusive Scan
 // ----------------------------------------------------------------------------------
-template<class ForwardIt1, class ForwardIt2, class BinaryOperation>
-ForwardIt2 inclusive_scan(ForwardIt1      first,
-                          ForwardIt1      last,
-                          ForwardIt2      d_first,
-                          BinaryOperation binary_op)
+template<class InputIt, class OutputIt, class BinaryOperation>
+OutputIt
+inclusive_scan(InputIt first, InputIt last, OutputIt d_first, BinaryOperation binary_op)
 {
-    using InputType  = typename std::iterator_traits<ForwardIt1>::value_type;
-    using OutputType = typename std::iterator_traits<ForwardIt2>::value_type;
+    using InputType  = typename std::iterator_traits<InputIt>::value_type;
+    using OutputType = typename std::iterator_traits<OutputIt>::value_type;
     static_assert(std::is_convertible<InputType, OutputType>::value,
                   "Input type must be convertible to output type!");
 
@@ -32,13 +30,13 @@ ForwardIt2 inclusive_scan(ForwardIt1      first,
     return d_first;
 }
 
-template<class ForwardIt1, class ForwardIt2>
-ForwardIt2 inclusive_scan(ForwardIt1 first, ForwardIt1 last, ForwardIt2 d_first)
+template<class InputIt, class OutputIt>
+OutputIt inclusive_scan(InputIt first, InputIt last, OutputIt d_first)
 {
     return naive::sequential::inclusive_scan(first, last, d_first, std::plus<>());
 }
 
-template<class ForwardIt1> ForwardIt1 inclusive_scan(ForwardIt1 first, ForwardIt1 last)
+template<class InputIt> InputIt inclusive_scan(InputIt first, InputIt last)
 {
     return naive::sequential::inclusive_scan(first, last, first, std::plus<>());
 }
@@ -47,15 +45,12 @@ template<class ForwardIt1> ForwardIt1 inclusive_scan(ForwardIt1 first, ForwardIt
 //  Exclusive Scan
 // ----------------------------------------------------------------------------------
 
-template<class ForwardIt1, class ForwardIt2, class T, class BinaryOperation>
-ForwardIt2 exclusive_scan(ForwardIt1      first,
-                          ForwardIt1      last,
-                          ForwardIt2      d_first,
-                          T               init,
-                          BinaryOperation binary_op)
+template<class InputIt, class OutputIt, class T, class BinaryOperation>
+OutputIt exclusive_scan(
+    InputIt first, InputIt last, OutputIt d_first, T init, BinaryOperation binary_op)
 {
-    using InputType  = typename std::iterator_traits<ForwardIt1>::value_type;
-    using OutputType = typename std::iterator_traits<ForwardIt2>::value_type;
+    using InputType  = typename std::iterator_traits<InputIt>::value_type;
+    using OutputType = typename std::iterator_traits<OutputIt>::value_type;
     static_assert(std::is_convertible<InputType, OutputType>::value,
                   "Input type must be convertible to output type!");
     static_assert(std::is_same<InputType, T>::value,
@@ -70,14 +65,14 @@ ForwardIt2 exclusive_scan(ForwardIt1      first,
     return d_first;
 }
 
-template<class ForwardIt1, class ForwardIt2, class T>
-ForwardIt2 exclusive_scan(ForwardIt1 first, ForwardIt1 last, ForwardIt2 d_first, T init)
+template<class InputIt, class OutputIt, class T>
+OutputIt exclusive_scan(InputIt first, InputIt last, OutputIt d_first, T init)
 {
     return naive::sequential::exclusive_scan(first, last, d_first, init, std::plus<>());
 }
 
-template<class ForwardIt1, class T>
-ForwardIt1 exclusive_scan(ForwardIt1 first, ForwardIt1 last, T init)
+template<class InputIt, class T>
+InputIt exclusive_scan(InputIt first, InputIt last, T init)
 {
     return naive::sequential::exclusive_scan(first, last, first, init, std::plus<>());
 }
@@ -86,17 +81,17 @@ ForwardIt1 exclusive_scan(ForwardIt1 first, ForwardIt1 last, T init)
 //  Inclusive Segmented Scan
 // ----------------------------------------------------------------------------------
 
-template<class ForwardIt1, class ForwardIt2, class ForwardIt3, class BinaryOperation>
-ForwardIt2 inclusive_segmented_scan(ForwardIt1      first,
-                                    ForwardIt1      last,
-                                    ForwardIt3      b_first,
-                                    ForwardIt2      d_first,
-                                    BinaryOperation binary_op)
+template<class InputIt, class OutputIt, class FlagIt, class BinaryOperation>
+OutputIt inclusive_segmented_scan(InputIt         first,
+                                  InputIt         last,
+                                  FlagIt          flag_first,
+                                  OutputIt        d_first,
+                                  BinaryOperation binary_op)
 {
-    using InputType  = typename std::iterator_traits<ForwardIt1>::value_type;
-    using BoolType   = typename std::iterator_traits<ForwardIt3>::value_type;
-    using OutputType = typename std::iterator_traits<ForwardIt2>::value_type;
-    static_assert(std::is_convertible<BoolType, bool>::value,
+    using InputType  = typename std::iterator_traits<InputIt>::value_type;
+    using FlagType   = typename std::iterator_traits<FlagIt>::value_type;
+    using OutputType = typename std::iterator_traits<OutputIt>::value_type;
+    static_assert(std::is_convertible<FlagType, bool>::value,
                   "Second Input Iterator type must be convertible to bool!");
     static_assert(std::is_convertible<InputType, OutputType>::value,
                   "Input type must be convertible to output type!");
@@ -105,55 +100,50 @@ ForwardIt2 inclusive_segmented_scan(ForwardIt1      first,
 
     for (auto i = first; i != last; i++)
     {
-        if (*(b_first++))
+        if (*(flag_first++))
         {
-            sum = binary_op(sum, *i);
+            sum = *i;
         }
         else
         {
-            sum = *i;
+            sum = binary_op(sum, *i);
         }
         *(d_first++) = sum;
     }
     return d_first;
 }
 
-template<class ForwardIt1, class ForwardIt2, class ForwardIt3>
-ForwardIt2 inclusive_segmented_scan(ForwardIt1 first,
-                                    ForwardIt1 last,
-                                    ForwardIt3 b_first,
-                                    ForwardIt2 d_first)
+template<class InputIt, class OutputIt, class FlagIt>
+OutputIt
+inclusive_segmented_scan(InputIt first, InputIt last, FlagIt flag_first, OutputIt d_first)
 {
     return naive::sequential::inclusive_segmented_scan(
-        first, last, d_first, std::plus<>());
+        first, last, flag_first, d_first, std::plus<>());
 }
 
-template<class ForwardIt1, class ForwardIt3>
-ForwardIt1 inclusive_segmented_scan(ForwardIt1 first, ForwardIt1 last, ForwardIt3 b_first)
+template<class InputIt, class FlagIt>
+InputIt inclusive_segmented_scan(InputIt first, InputIt last, FlagIt flag_first)
 {
-    return naive::sequential::inclusive_segmented_scan(first, last, first, std::plus<>());
+    return naive::sequential::inclusive_segmented_scan(
+        first, last, flag_first, first, std::plus<>());
 }
 
 // ----------------------------------------------------------------------------------
 //  Exclusive Segmented Scan
 // ----------------------------------------------------------------------------------
 
-template<class ForwardIt1,
-         class ForwardIt2,
-         class ForwardIt3,
-         class BinaryOperation,
-         class T>
-ForwardIt2 exclusive_segmented_scan(ForwardIt1      first,
-                                    ForwardIt1      last,
-                                    ForwardIt3      b_first,
-                                    ForwardIt2      d_first,
-                                    T               init,
-                                    BinaryOperation binary_op)
+template<class InputIt, class OutputIt, class FlagIt, class BinaryOperation, class T>
+OutputIt exclusive_segmented_scan(InputIt         first,
+                                  InputIt         last,
+                                  FlagIt          flag_first,
+                                  OutputIt        d_first,
+                                  T               init,
+                                  BinaryOperation binary_op)
 {
-    using InputType  = typename std::iterator_traits<ForwardIt1>::value_type;
-    using BoolType   = typename std::iterator_traits<ForwardIt3>::value_type;
-    using OutputType = typename std::iterator_traits<ForwardIt2>::value_type;
-    static_assert(std::is_convertible<BoolType, bool>::value,
+    using InputType  = typename std::iterator_traits<InputIt>::value_type;
+    using FlagType   = typename std::iterator_traits<FlagIt>::value_type;
+    using OutputType = typename std::iterator_traits<OutputIt>::value_type;
+    static_assert(std::is_convertible<FlagType, bool>::value,
                   "Second Input Iterator type must be convertible to bool!");
     static_assert(std::is_convertible<InputType, OutputType>::value,
                   "Input type must be convertible to output type!");
@@ -164,33 +154,32 @@ ForwardIt2 exclusive_segmented_scan(ForwardIt1      first,
 
     for (auto i = first; i != last; i++)
     {
-        if (*(b_first++))
-        {
-            sum = binary_op(sum, *i);
-        }
-        else
+        *(d_first++) = sum;
+        if (*(flag_first++))
         {
             sum = *i;
         }
-        *(d_first++) = sum;
+        else
+        {
+            sum = binary_op(sum, *i);
+        }
     }
     return d_first;
 }
 
-template<class ForwardIt1, class ForwardIt2, class ForwardIt3, class T>
-ForwardIt2 exclusive_segmented_scan(
-    ForwardIt1 first, ForwardIt1 last, ForwardIt3 b_first, ForwardIt2 d_first, T init)
+template<class InputIt, class OutputIt, class FlagIt, class T>
+OutputIt exclusive_segmented_scan(
+    InputIt first, InputIt last, FlagIt flag_first, OutputIt d_first, T init)
 {
     return naive::sequential::exclusive_segmented_scan(
-        first, last, d_first, init, std::plus<>());
+        first, last, flag_first, d_first, init, std::plus<>());
 }
 
-template<class ForwardIt1, class ForwardIt3, class T>
-ForwardIt1
-exclusive_segmented_scan(ForwardIt1 first, ForwardIt1 last, ForwardIt3 b_first, T init)
+template<class InputIt, class FlagIt, class T>
+InputIt exclusive_segmented_scan(InputIt first, InputIt last, FlagIt flag_first, T init)
 {
     return naive::sequential::exclusive_segmented_scan(
-        first, last, first, init, std::plus<>());
+        first, last, flag_first, first, init, std::plus<>());
 }
 
 }; // namespace sequential
--- test/test.cpp
+++ test/test.cpp
@@ -58,3 +58,53 @@ TEST_CASE("Exclusive Scan Test")
     // SECTION("Naive Up-Down-Sweep") { ; }
     // SECTION("Naive Tiled") { ; }
 }
+
+TEST_CASE("Inclusive Segmented Scan Test")
+{
+    // Test parameters
+    size_t N = 20;
+    // Logging of parameters
+    CAPTURE(N);
+
+    std::vector<int> data(
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20});
+    std::vector<int> flags({0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1});
+    std::vector<int> reference(
+        {1, 3, 3, 4, 5, 6, 7, 8, 17, 27, 38, 12, 13, 14, 15, 31, 48, 18, 37, 57});
+
+    // Tests
+    SECTION("Naive Sequential")
+    {
+        std::vector<int> result(N, 0);
+        naive::sequential::inclusive_segmented_scan(
+            data.begin(), data.end(), flags.begin(), result.begin());
+        REQUIRE_THAT(result, Catch::Matchers::Equals(reference));
+    }
+    // SECTION("Naive Up-Down-Sweep") { ; }
+    // SECTION("Naive Tiled") { ; }
+}
+
+TEST_CASE("Exclusive Segmented Scan Test")
+{
+    // Test parameters
+    size_t N = 20;
+    // Logging of parameters
+    CAPTURE(N);
+
+    std::vector<int> data(
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20});
+    std::vector<int> flags({0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1});
+    std::vector<int> reference(
+        {0, 1, 3, 3, 7, 5, 11, 7, 15, 24, 34, 11, 12, 13, 14, 29, 45, 17, 37, 56});
+
+    // Tests
+    SECTION("Naive Sequential")
+    {
+        std::vector<int> result(N, 0);
+        naive::sequential::exclusive_segmented_scan(
+            data.begin(), data.end(), flags.begin(), result.begin(), 0);
+        REQUIRE_THAT(result, Catch::Matchers::Equals(reference));
+    }
+    // SECTION("Naive Up-Down-Sweep") { ; }
+    // SECTION("Naive Tiled") { ; }
+}
