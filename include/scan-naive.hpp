d99d6ab8d1069ec7dcd096cedd297b338c698b2f
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2022-01-08 17:34:05 +0100

    Removed superfluous code and added comments.

--- include/scan-naive.hpp
+++ include/scan-naive.hpp
@@ -83,6 +83,11 @@ OutputIt inclusive_segmented_scan(InputIt         first,
     static_assert(std::is_convertible<PairType, OutputType>::value,
                   "Input type must be convertible to output type!");
 
+    /*Sequential inclusive scan becomes the segmented variant by wrapping the
+      passed binary_op into a new conditional binary operation. Assuming
+      a addition with the running sum as operand x and the current value as
+      operand y, resetting the sum to the value of y yields the correct result.
+     */
     return naive::sequential::inclusive_scan(first,
                                              last,
                                              d_first,
@@ -93,10 +98,13 @@ OutputIt inclusive_segmented_scan(InputIt         first,
                                                  {
                                                      result.first =
                                                          binary_op(x.first, y.first);
-                                                     if (x.second)
-                                                     {
-                                                         result.second = x.second;
-                                                     }
+                                                     /* Only required if additions are
+                                                        reordered !
+                                                      */
+                                                     // if (x.second)
+                                                     // {
+                                                     //     result.second = x.second;
+                                                     // }
                                                  }
                                                  return result;
                                              });
@@ -130,6 +138,9 @@ OutputIt exclusive_segmented_scan(
     static_assert(std::is_convertible<PairType, OutputType>::value,
                   "Input type must be convertible to output type!");
 
+    /*Sequential exclusive scan becomes the segmented variant by wrapping the
+      passed binary_op into a new conditional binary like with inclusive scan.
+    */
     naive::sequential::exclusive_scan(first,
                                       last,
                                       d_first,
@@ -140,13 +151,20 @@ OutputIt exclusive_segmented_scan(
                                           if (!y.second)
                                           {
                                               result.first = binary_op(x.first, y.first);
-                                              if (x.second)
-                                              {
-                                                  result.second = x.second;
-                                              }
+                                              /* Only required if additions are
+                                                 reordered!
+                                              */
+                                              // if (x.second)
+                                              // {
+                                              //     result.second = x.second;
+                                              // }
                                           }
                                           return result;
                                       });
+
+    // Reset of segment beginnings to initial value.
+    // Using only an operand wrapper it is not possible to omit this step!
+    // See implementation under numeric.h
     while (first != last)
     {
         if (first->second)
@@ -301,6 +319,20 @@ OutputIt inclusive_segmented_scan(InputIt         first,
                                   OutputIt        d_first,
                                   BinaryOperation binary_op)
 {
+    /*Up-Down inclusive scan becomes the segmented variant by wrapping the
+      passed binary_op into a new conditional binary operation. Assuming
+      a addition with the running sum as operand x and the current value as
+      operand y, resetting the sum to the value of y yields the correct result.
+
+      Copying of the flag is only required when executing non
+      sequentially. Since the up-sweep works on operands over segment
+      boundaries as the stages progress, segments already finished need to be
+      marked as such. In these cases, the flag of the right operand is set
+      to prevent further addition during later stages.
+
+      For the down sweep phase unset flags represent unfinished work rather
+      than actual segments.
+    */
     using PairType   = typename std::iterator_traits<InputIt>::value_type;
     using FlagType   = typename std::tuple_element<1, PairType>::type;
     using OutputType = typename std::iterator_traits<OutputIt>::value_type;
@@ -308,6 +340,7 @@ OutputIt inclusive_segmented_scan(InputIt         first,
                   "Second Input Iterator type must be convertible to bool!");
     static_assert(std::is_convertible<PairType, OutputType>::value,
                   "Input type must be convertible to output type!");
+
     return naive::updown::inclusive_scan(first,
                                          last,
                                          d_first,
@@ -318,6 +351,9 @@ OutputIt inclusive_segmented_scan(InputIt         first,
                                              {
                                                  result.first =
                                                      binary_op(x.first, y.first);
+                                                 // Since additions are reordered
+                                                 // flags need to be carried along
+                                                 // to indicate finished segments!
                                                  if (x.second)
                                                  {
                                                      result.second = x.second;
@@ -346,7 +382,9 @@ template<class InputIt, class OutputIt, class BinaryOperation, class T>
 OutputIt exclusive_segmented_scan(
     InputIt first, InputIt last, OutputIt d_first, T init, BinaryOperation binary_op)
 {
-
+    /* Due to the add-swap operation in the down sweep phase a simple wrapper
+       of the binary operation is insufficient.
+     */
     using PairType   = typename std::iterator_traits<InputIt>::value_type;
     using FlagType   = typename std::tuple_element<1, PairType>::type;
     using OutputType = typename std::iterator_traits<OutputIt>::value_type;
@@ -357,12 +395,17 @@ OutputIt exclusive_segmented_scan(
 
     size_t num_values = last - first;
     size_t step       = 1;
-    // Up sweep
 
     if (std::distance(first, d_first) != 0)
     {
         std::copy(first, last, d_first);
     }
+
+    /* Up sweep
+       Essentially the same operation as with the conditional wrapper but
+       now unpacked. This step is hence equal to the segmented inclusive
+       up-down sweeping scan.
+    */
     for (size_t stage = 0; stage < std::floor(std::log2(num_values)); stage++)
     {
         step = step * 2;
@@ -381,28 +424,44 @@ OutputIt exclusive_segmented_scan(
         }
     }
     d_first[num_values - 1] = std::make_pair(init, 0);
-
     for (int stage = std::floor(std::log2(num_values)) - 1; stage >= 0; stage--)
     {
         for (size_t i = 0; i < num_values; i = i + (1 << (stage + 1)))
         {
             size_t left = i + (1 << stage) - 1, right = i + (1 << (stage + 1)) - 1;
+            /*Add-Swap Operation needs to be handled differently than with all the
+              other variants. Flags from the previous phase remain unmodified.
+              There are two cases to be observed.
+             */
             if (not d_first[left].second)
             {
+                /*Left operand is not a segment beginning:
+                  Independent of the right operand, swap the left value
+                  with the right and save the sum in the right operand.
+
+                  The segment beginnings will be reset to the init value at
+                  a later stage and are currently required to hold intermediate
+                  results.
+                 */
                 OutputType t         = d_first[left];
                 d_first[left].first  = d_first[right].first;
                 d_first[right].first = binary_op(t.first, d_first[right].first);
             }
             else
             {
-
+                /*Left operand is a segment beginning:
+                  Independent of the right operand, swap the operand values.
+                  This rule moves the already correctly calculated values into
+                  their right place.
+                 */
                 OutputType t         = d_first[left];
                 d_first[left].first  = d_first[right].first;
                 d_first[right].first = t.first;
             }
         }
     }
-
+    // Like with all exclusives variants, this step cannot be omitted or
+    // fused with any previous operation.
     while (first != last)
     {
         if (first->second)
@@ -455,7 +514,6 @@ inclusive_scan(InputIt first, InputIt last, OutputIt d_first, BinaryOperation bi
     // Phase 1: Reduction
     for (size_t i = 0; i < num_tiles; i++)
     {
-
         temp[i] = *(first + i * tile_size);
         for (size_t j = 1 + i * tile_size; j < 1 + (i + 1) * tile_size; j++)
         {
